0b0f15c9ddec5c2d9bbc39814a9178db5e1b2251
Fi Zilalil Huda <76860149+akiyamanico@users.noreply.github.com>
2024-05-31 18:27:48 +0700

    trigger condition "tersedia" by frontend

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -5,34 +5,44 @@ Travel Haji Gabut | Halaman Tiket
 @endsection
 @section('content')
 <div class="dark:bg-black">
-    <div class="pt-28 mx-auto max-w-7xl sm:px-6 lg:px-8 pb-16">
+    <div class="pt-28">
+
         @foreach ($paketdata as $i => $a)
-        <div
-            class="relative dark:bg-gradient-to-r from-black to-gray-500 flex items-center justify-between h-48 bg-white lg:rounded-md lg:shadow-lg  lg:px-8 lg:py-6">
-            <div class="flex-shrink-0">
-                    <a href="{{route('produkdetail', $a->id)}}" title=""
-                        class="dark:text-white text-base text-xl text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600 font-helmed">
-                        <span>{{$a->namapaket}}</span>
-                    </a>
-                    <div class="font-quicksand flex flex-col">
-                        <span>Jadwal Keberangkatan : {{$a->tanggalberangkat}} </span>
-                        <span>Durasi Paket : {{$a->durasi}}</span>
-                        <span>Berangkat Dari : {{$a->asalkeberangkatan}}</span>
-                    </div>
-               
-            </div>
-            <div class="hidden lg:flex lg:items-center lg:space-x-10">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 pb-7">
                 <div
-                    class="dark:text-white  font-quicksand font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
-                    <span>
-                        Harga :
-                    </span>
-                    <span>
-                        {{$a->harga}}
-                    </span>
+                    class="relative dark:bg-gradient-to-r from-black to-gray-500 flex items-center justify-between h-36 bg-white lg:rounded-md lg:shadow-lg lg:px-4">
+                    <div class="flex-shrink-0">
+                        @if($a->tersedia === 'Tidak Tersedia')
+                            <a href="#" title=""
+                                class="dark:text-red-400  text-xl text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600 font-helmed">
+                                <span>{{$a->namapaket}} || Sold Out!</span>
+                            </a>
+                        @else
+                            <a href="{{route('produkdetail', $a->id)}}" title=""
+                                class="dark:text-white  text-xl text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600 font-helmed">
+                                <span>{{$a->namapaket}}</span>
+                            </a>
+                        @endif
+                        <div class="font-quicksand flex flex-col">
+                            <span>Jadwal Keberangkatan : {{$a->tanggalberangkat}} </span>
+                            <span>Durasi Paket : {{$a->durasi}}</span>
+                            <span>Berangkat Dari : {{$a->asalkeberangkatan}}</span>
+                        </div>
+
+                    </div>
+                    <div class="hidden lg:flex lg:items-center lg:space-x-10">
+                        <div
+                            class="dark:text-white  font-quicksand font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                            <span>
+                                Harga :
+                            </span>
+                            <span>
+                                {{$a->harga}}
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         @endforeach
     </div>
 </div>
